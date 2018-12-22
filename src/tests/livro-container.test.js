dfabf1f463e70aec298dc1274661dd1d2ff53ea7
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2018-12-22 18:46:17 -0200

    implementado prototipo de Novo Livro e tests

--- src/tests/livro-container.test.js
+++ src/tests/livro-container.test.js
@@ -30,5 +30,51 @@ describe('test LivroConteiner component', () => {
     expect(isbn).toHaveProp('name', 'isbn');
   });
 
+  it('should be input Preço', () => {
+    const preco = wrapper.find('#livroPreco');
+    expect(preco).toMatchSelector('input');
+    expect(preco).toHaveProp('name', 'preco');
+  });
+
+  it('should be input Data Lançamento', () => {
+    const dataLancamento = wrapper.find('#livroDataLancamento');
+    expect(dataLancamento).toMatchSelector('input');
+    expect(dataLancamento).toHaveProp('name', 'dataLancamento');
+  });
+
+  it('should be multiselect Autor', () => {
+    const autores = wrapper.find('#livroAutores');
+    expect(autores).toMatchSelector('select');
+    expect(autores).toHaveProp('name', 'selectAutor');
+  });
+
+  it('should be button adicionar autor', () => {
+    const btnAutor = wrapper.find('#btnAddAutor');
+    expect(btnAutor).toMatchSelector('button');
+    expect(btnAutor).toHaveProp('type', 'button');
+    expect(btnAutor).toHaveText('Adicionar Autor');
+  });
+
+  it('should be form to submit livro', () => {
+    expect(wrapper.find('form')).toExist();
+  });
+
+  it('should be button submit Gravar Livro', () => {
+    const button = wrapper.find('#btnGravar');
+    expect(button).toMatchSelector('button');
+    expect(button).toHaveProp('type', 'submit');
+    expect(button).toHaveText('Gravar Livro');
+  });
+
+  it('should be table with columns Titulo, ISBN, Preço, Data Lançamento', () => {
+    const table = wrapper.find('table');
+    const thead = table.find('thead').find('tr').find('th');
+    expect(table.find('caption')).toHaveText('Livros');
+    expect(thead.at(0)).toHaveText('Titulo');
+    expect(thead.at(1)).toHaveText('ISBN');
+    expect(thead.at(2)).toHaveText('Preço');
+    expect(thead.at(3)).toHaveText('Data Lançamento');
+  });
+
 
 });
