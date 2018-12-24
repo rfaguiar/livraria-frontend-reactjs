8e144262a1b293baab17308d4ae956003f241c37
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2018-12-23 22:24:01 -0200

    tests de links

--- src/livraria-header/menu-container.jsx
+++ src/livraria-header/menu-container.jsx
@@ -8,7 +8,7 @@ const MenuContainer = () => {
         <li><a id={'livroItem'} href={'/livros'}>Livros</a></li>
         <li><a id={'autorItem'} href={'/autores'}>Autores</a></li>
         <li><a id={'vendaItem'} href={'/vendas'}>Vendas</a></li>
-        <li><a id={'logoutItem'} href={'/logout'}>Logout</a></li>
+        <li><a id={'logoutItem'} href={'/login'}>Logout</a></li>
       </ul>
     </nav>
   );
--- src/tests/menu-container.test.js
+++ src/tests/menu-container.test.js
@@ -18,25 +18,29 @@ describe('test MenuContainer component', () => {
   it('should be menu item Livro', () => {
     const livro = wrapper.find('#livroItem');
     expect(livro).toMatchSelector('a');
-    expect(livro).toHaveText('Livros')
+    expect(livro).toHaveText('Livros');
+    expect(livro).toHaveProp('href', '/livros');
   });
 
   it('should be menu item Autores', () => {
     const autor = wrapper.find('#autorItem');
     expect(autor).toMatchSelector('a');
     expect(autor).toHaveText('Autores');
+    expect(autor).toHaveProp('href', '/autores');
   });
 
   it('should be menu item Vendas', () => {
     const venda = wrapper.find('#vendaItem');
     expect(venda).toMatchSelector('a');
     expect(venda).toHaveText('Vendas');
+    expect(venda).toHaveProp('href', '/vendas');
   });
 
   it('should be menu item Logout', () => {
     const logout = wrapper.find('#logoutItem');
     expect(logout).toMatchSelector('a');
     expect(logout).toHaveText('Logout');
+    expect(logout).toHaveProp('href', '/login');
   });
 
 });
