ba76e80cb17dcd2df37f759ea238fb09b6cd06af
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2018-12-24 10:03:13 -0200

    fix browserrouter

--- src/livraria-router/index.js
+++ src/livraria-router/index.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import  {Router, Switch} from 'react-router-dom';
-import createHashHistory from 'history/createHashHistory';
+import  {BrowserRouter, Switch} from 'react-router-dom';
 import Login from '../components/login';
 import Vendas from '../components/venda';
 import Autor from '../components/autor';
 import Livro from '../components/livro';
 import DefaultLayoutProtected from './default-layout-protected';
 
-const hashHistory = createHashHistory({ basename: process.env.PUBLIC_URL });
-
 const LivrariaRouter = () => {
   return (
-    <Router history={hashHistory}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Switch>
         <DefaultLayoutProtected path={'/livros'} exact component={Livro}/>
         <DefaultLayoutProtected path={'/autores'} exact component={Autor}/>
         <DefaultLayoutProtected path={'/vendas'} exact component={Vendas}/>
         <DefaultLayoutProtected path={'*'} exact component={Login}/>
       </Switch>
-    </Router>
+    </BrowserRouter>
   );
 };
 
