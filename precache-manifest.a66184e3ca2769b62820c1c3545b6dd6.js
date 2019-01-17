5330df3c2a7f8318aaa9d84ff8cf381145b8d62a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-17 00:39:02 +0000

    Deploy rfaguiar/livraria-frontend-reactjs to github.com/rfaguiar/livraria-frontend-reactjs.git:gh-pages

---
+++ precache-manifest.a66184e3ca2769b62820c1c3545b6dd6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2ca6bd6c91a1ee6aff5",
+    "url": "/livraria-frontend-reactjs/static/css/main.326ec1a1.chunk.css"
+  },
+  {
+    "revision": "e2ca6bd6c91a1ee6aff5",
+    "url": "/livraria-frontend-reactjs/static/js/main.e2ca6bd6.chunk.js"
+  },
+  {
+    "revision": "80461921f77b5292f6f5",
+    "url": "/livraria-frontend-reactjs/static/js/1.80461921.chunk.js"
+  },
+  {
+    "revision": "ae899c2d9caf3e3e0866",
+    "url": "/livraria-frontend-reactjs/static/js/runtime~main.ae899c2d.js"
+  },
+  {
+    "revision": "d9bac51ef96b70adbbd17632bbedcb92",
+    "url": "/livraria-frontend-reactjs/index.html"
+  }
+];
